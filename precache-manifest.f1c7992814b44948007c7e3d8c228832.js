5d130d1cb2dfa76cba502658e8b362299ef22967
Archakov Dennis <amonbower@gmail.com>
2020-08-29 15:50:32 +0300

    Updates

---
+++ precache-manifest.f1c7992814b44948007c7e3d8c228832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eceba4ac7a136acde8789babfda4445",
+    "url": "/todo-react-youtube/index.html"
+  },
+  {
+    "revision": "e2f10f44b3d4319c994b",
+    "url": "/todo-react-youtube/static/css/main.173cfa3b.chunk.css"
+  },
+  {
+    "revision": "c69c5e18b3c8fa0059cb",
+    "url": "/todo-react-youtube/static/js/2.c25c4c47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react-youtube/static/js/2.c25c4c47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f10f44b3d4319c994b",
+    "url": "/todo-react-youtube/static/js/main.e19a0fcf.chunk.js"
+  },
+  {
+    "revision": "dbc105db51c07a5f2201",
+    "url": "/todo-react-youtube/static/js/runtime-main.b9391e2e.js"
+  }
+]);
